41134d0227cb96c1b71f0bcbb3546290586a295b
chengwentao <chengwentao@moxigame.cn>
2020-07-30 16:19:12 +0800

    7.30 在排序数组中查找元素的第一个和最后一个出现的位置

---
+++ LeetCode每日一题/2020.7.30——0034.在排序数组中查找元素的第一个和最后一个位置/LeetCode-0034.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        public int[] Serach(int[] nums, int target)
+        {
+            int i = 0;
+            int j = nums.Length - 1;
+
+            //搜索右边界 right
+            while (i <= j)
+            {
+                int m = (i + j) / 2;
+                if (nums[m] <= target)
+                {
+                    i = m + 1;
+                }
+                else
+                {
+                    j = m - 1;
+                }
+
+            }
+            int right = i;
+
+
+            // 搜索左边界 left
+            i = 0; j = nums.Length - 1;
+            while (i <= j)
+            {
+                int m = (i + j) / 2;
+                if (nums[m] < target) i = m + 1;
+                else j = m - 1;
+            }
+            int left = j;
+
+            return new int[] { left, right };
+
+        }
+    }
+}
