36d2001f9a0925361ccd8923ce0902b338b3cc5b
chengwentao <chengwentao@moxigame.cn>
2020-08-10 17:15:05 +0800

    2020.08.10——#0805递归乘法

---
+++ LeetCode每日一题/2020.08.10——#0805递归乘法/LeetCode-#0805.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+
+        public int Multiply(int a, int b)
+        {
+            //不用*  实现a*b
+
+            //1.循环 a 遍  每次增加 b
+
+            //2.递归解法  （同理与循环） 
+
+            if (A > B)
+            {
+                return Multiply(B, A);   //确保递归的次数最少
+            }
+
+
+            if (A == 0)
+            {
+                return 0;   //递归终止条件
+            }
+            return B + Multiply(A - 1, B);   //累加
+
+
+        }
+    }
+}
