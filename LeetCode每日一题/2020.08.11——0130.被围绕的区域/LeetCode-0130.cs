a6da2f5bc484bbd9004a07a66d5fc29bb769bb05
zxczxc1234 <214429956@qq.com>
2020-08-16 20:35:02 +0800

    2020.08.16——review

--- LeetCode每日一题/2020.08.11——0130.被围绕的区域/LeetCode-0130.cs
+++ LeetCode每日一题/2020.08.11——0130.被围绕的区域/LeetCode-0130.cs
@@ -1,79 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApp1
-{
-    class Program
+﻿namespace ConsoleApp1
     {
-
-        public int col;
-        public int row;
-
-        public void Solve(char[][] board)
+        class Program
         {
-            //col 列  row 行
-            int col = board.Length;
-            int row = board[0].Length;
 
-            //该题目需要将所有被X包围的O找出来，那剩下的O就是连接起来与边界连通的O。
-            //直接找出来所有被X包围的O并不好找，但是可以用排除法：
-            //先找到所有连接起来与边界连通的O，将这些O标记一下，然后遍历数组，所有没有被标记的O就是我们要找的O。
+            public int row;
+            public int col;
 
-            //边界的行
-            for (int i = 0; i < row; i++)
+            public void Solve(char[][] board)
             {
-                //最后一行
-                DFS(board, i, row - 1);
-                //第一行
-                DFS(board, i, 0);
-            }
-
-            //边界的列
-            for (int j = 0; j < col; j++)
-            {
-                //第一列
-                DFS(board, 0, j);
-                //最后一列
-                DFS(board, row - 1,j);
-            }
-
-            //此时整个二维数组已经标记完成
-            //再遍历整个数组将所有的'O'改为'X' （此时'O'即为符合要求的被围绕区域）
-            //将所有的'A'改为'O'   (此时所有的'A'即为所有与边界连接的'O')
-
-            for (int i = 0; i < row; i++)
-            {
-                for (int j = 0; j < col; j++)
+                if (board.Length == 0) return;
+                int col = board.Length;
+                int row = board[0].Length;
+                // 先遍历所有边界为O的点
+                for (int i = 0; i < col; i++)
                 {
-                    if (board[i][j] == 'O')
+                    dfs(board, i, 0, col, row);
+                    dfs(board, i, row - 1, col, row);
+                }
+                for (int j = 0; j < row; j++)
+                {
+                    dfs(board, 0, j, col, row);
+                    dfs(board, col - 1, j, col, row);
+                }
+
+                for (int i = 0; i < col; i++)
+                {
+                    for (int j = 0; j < row; j++)
                     {
-                        board[i][j] == 'X';
-                    }
-                    if (board[i][j] == 'A')
-                    {
-                        board[i][j] = 'O';
+                        if (board[i][j] == 'O')
+                        {
+                            board[i][j] = 'X';
+                        }
+                        if (board[i][j] == '*')
+                        {
+                            board[i][j] = 'O';
+                        }
                     }
                 }
             }
-    
-        }
 
-        public void DFS(char[][] board, int i, int j)
-        {
-            if (i < 0 || j < 0 || i >= row || j >= col || board[i][j] != 'O')
+            private void dfs(char[][] board, int i, int j, int col, int row)
             {
-                return;
+                if (i > col - 1 || j > row - 1 || i < 0 || j < 0) return;
+                if (board[i][j] != 'O') return;
+                board[i][j] = '*';
+                // 找到相连的点, 上下左右
+                dfs(board, i - 1, j, col, row);
+                dfs(board, i + 1, j, col, row);
+                dfs(board, i, j - 1, col, row);
+                dfs(board, i, j + 1, col, row);
             }
-            
-            board[i][j] = 'A';
-            DFS(board[i][j + 1]);
-            DFS(board[i][j - 1]);
-            DFS(board[i+1][j]);
-            DFS(board[i-1][j]);
-            return;
+
+
         }
     }
+
+}
 }
--- LeetCode每日一题/2020.08.13——0043.字符串相乘/LeetCode-0043.cs
+++ LeetCode每日一题/2020.08.13——0043.字符串相乘/LeetCode-0043.cs
@@ -12,57 +12,59 @@ namespace ConsoleApp1
         public string Multiply(string nums1, string nums2)
         {
 
-            if (nums1 == "0" || nums2 == "0")
-            {
-                return "0";
-            }
+            int m = num1.Length;
+            int n = num2.Length;
 
-            int m = nums1.Length;
-            int n = nums2.Length;
+            if (num1 == "0" || num2 == "0") return "0";
 
-            //将字符串转化为数组
+            //字符串转化为数组
             int[] n1 = new int[m];
             int[] n2 = new int[n];
 
-            int[] pos = new int[m + n];
-
             for (int i = 0; i < m; i++)
             {
-                n1[i] = nums1[i] - '0';
+                n1[i] = num1[i] - '0';
             }
 
             for (int i = 0; i < n; i++)
             {
-                n2[i] = nums2[i] - '0';
+                n2[i] = num2[i] - '0';
             }
 
+            //1.声明一个新的数组来存放两数相乘的结果
+            int[] nums = new int[m + n];
 
-            for (int i = n1.Length - 1; i >= 0; i--)
+            //从后开始存入  模拟数字相乘的过程
+            for (int i = m - 1; i >= 0; i--)
             {
-                for (int j = n2.Length - 1; j >= 0; j--)
+                for (int j = n - 1; j >= 0; j--)
                 {
-                    //将两数的乘积 分别加入到数组中
-
-                    pos[i + j + 1] += n1[i] * n2[j];
+                    //为什么要+1   因为定义的新数组长度为i+j  而 i*j 结果的长度一定小于 i+j
+                    //如果不+1 则新数组中的最后一位为0 相当于把原结果扩大了10倍
+                    nums[i + j + 1] += n1[i] * n2[j];
                 }
             }
 
-            //进行统一的进位操作
-            for (int i = pos.Length - 1; i >= 1; i--)
+            //统一进位操作
+            for (int i = nums.Length - 1; i >= 1; i--)
             {
-                pos[i - 1] += pos[i] / 10;
-                pos[i] = pos[i] % 10;
+
+                nums[i - 1] += nums[i] / 10;
+                nums[i] = nums[i] % 10;
+
             }
 
+            //进位完成之后去除首部的0
             StringBuilder sb = new StringBuilder();
 
-            for (int i = 0; i < pos.Length; i++)
+            for (int i = 0; i < nums.Length; i++)
             {
-                sb.Append(pos[i]);
+                sb.Append(nums[i]);
             }
 
             string res = sb.ToString().TrimStart('0');
             return res.Length == 0 ? "0" : res;
+
 
 
         }
