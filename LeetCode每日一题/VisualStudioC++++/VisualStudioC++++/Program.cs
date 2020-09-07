1df22ebd73ad4c9532f8eef299ba72197d9417c8
chengwentao <chengwentao@moxigame.cn>
2020-09-07 14:12:11 +0800

    2020.09.07——update

--- LeetCode每日一题/VisualStudioC++++/VisualStudioC++++/Program.cs
+++ LeetCode每日一题/VisualStudioC++++/VisualStudioC++++/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,17 +11,38 @@ namespace VisualStudioC____
     {
         static void Main(string[] args)
         {
+            int[] nums = new int[] {1,1,2,1,2,2,1};
+            
+            Console.WriteLine();
+            Console.ReadKey();
         }
 
-        public static int WiggleMaxLength(int[] nums)
+        public char PredictTheWinner(string s ,string t)
         {
-            //1.原数组进行排序 —— 插入
-            //2. 找到数组中的中位数    将小于中位数的元素加入到数组A中  大于中位数的元素添加到数组B中
-            //    按照ABABABAB的顺序添加到最终的数组中        时间复杂度O（n）   空间复杂度O（n）
-            int length = nums.Length;
-            int[] left = new int[];
-            int[] right = new int[];
-            return 0;
+            Hashtable ha = new Hashtable();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!ha.Contains(s[i]))
+                {
+                    ha.Add(s[i], i);
+                }
+            }
+
+            for (int i = 0; i < t.Length; i++)
+            {
+                if (ha.Contains(t[i]))
+                {
+                    continue;
+                }
+                else
+                {
+                    return t[i];
+                }
+            }
+            return ' ';
         }
+
+
+
     }
 }
