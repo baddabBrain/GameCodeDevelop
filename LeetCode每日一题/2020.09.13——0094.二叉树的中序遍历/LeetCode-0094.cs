b646d1d888b22a8bb5e005921d8e969476bf9812
chengwentao <chengwentao@moxigame.cn>
2020-09-14 16:24:02 +0800

    2020.09.14——update

---
+++ LeetCode每日一题/2020.09.13——0094.二叉树的中序遍历/LeetCode-0094.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+
+
+
+        public List<int> InorderTraversal(TreeNode root)
+        {
+            List<int> res = new List<int>();
+            Stack<ColorNode> sta = new Stack<ColorNode>();
+            ColorNode node = new ColorNode(root, true);
+
+            sta.Push(node);
+
+            //中序遍历的顺序为 左根右
+            //所以入栈顺序应为 右根左  弹出顺序才能为左根右
+            while (sta.Count > 0)
+            {
+                ColorNode temp = sta.Pop();
+                if (temp == null) continue;
+
+                if (temp._visited == true)
+                {
+                    sta.Push(new ColorNode(temp._node.right, true));
+                    sta.Push(new ColorNode(temp._node, false));
+                    sta.Push(new ColorNode(temp._node.left, true));
+                }
+                else
+                {
+                    res.Add(temp._node.val);
+                }
+            }
+        }
+
+
+        //public List<int> InorderTraversal(TreeNode root)
+        //{
+        //    List<int> res = new List<int>();
+        //    Helper(root, res);
+        //    return res;
+
+        //}
+
+        //public void Helper(TreeNode node, List<int> list)
+        //{
+        //    //递归终止条件
+        //    if (node == null) return;
+
+        //    Helper(node.left, list);
+        //    list.Add(node.val);
+        //    Helper(node.right, list);
+
+        //}
+
+
+    }
+
+    public class ColorNode
+    {
+        public TreeNode _node;
+        public bool _visited;
+        public ColorNode(TreeNode node, bool value)
+        {
+            _node = node;
+            _visited = value;
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x )
+        {
+            val = x;
+        }
+    }
+}
